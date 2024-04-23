8d4e6c04e96fd8528a1ad8c6eae2f694c0bb6ccb
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-23 13:15:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  let r1=parseInt(document.getElementById("radius").value);
-	let volume=document.getElementById("volume");
+  let r=document.getElementById("radius");
+  let r1=parseInt(r.value);
+	let vol=document.getElementById("volume");
 		let v1=0;
 		const pi=3.14159265359;
 		if (r1>0) {
 			v1=(4*pi*r1**3)/3;
 			v1=v1.toFixed(4);
-		 volume.value=v1;
+		 vol.value=v1;
 		}
 		else{
-			volume.value="NaN";
+			vol.value="NaN";
 		}
 } 
 
