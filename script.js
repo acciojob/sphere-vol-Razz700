d916d809cf7a5012d360cba1aea0fcbe5ede5ecd
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-23 13:25:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function volume_sphere() {
     //Write your code here
   let r=document.getElementById("radius");
   let r1=parseInt(r.value);
-	let vol=document.getElementById("volume");
+	let volume=document.getElementById("volume");
 		let v1=0;
 		const pi=3.14159265359;
 		if (r1>0) {
 			v1=(4*pi*r1**3)/3;
 			v1=v1.toFixed(4);
-		 vol.value=v1;
+		 volume.value=v1;
 		}
 		else{
-			vol.value="NaN";
+			volume.value="NaN";
 		}
 } 
 
-/*window.onload = */document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload =document.getElementById('MyForm').onsubmit = volume_sphere;
